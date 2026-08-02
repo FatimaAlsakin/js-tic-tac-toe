34a74e5a2eeb1ef5d5bdab87eef5491ac3fac70a
FatimaAlsakin <falsakiin@gmail.com>
2026-08-02 20:42:52 +0300

    Cached Element

--- js/app.js
+++ js/app.js
@@ -9,7 +9,9 @@ let winner = null
 
 /*------------------------ Cached Element References ------------------------*/
 
-
+const sqrEl = document.querySelectorAll('.sqr')
+const borderEl = document.querySelectorAll('.board')
+const msgEl = document.querySelector('#message')
 
 /*-------------------------------- Functions --------------------------------*/
 
